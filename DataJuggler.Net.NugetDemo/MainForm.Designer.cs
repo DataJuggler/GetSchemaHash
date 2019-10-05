47c57f0c7b02791087127ca16eed2813be45b7a7
Data Juggler <corby@datajuggler.com>
2019-10-05 18:24:57 -0500

    This is the first commit of a simple Windows Form app that can be used to create a hash value reprsenting your database schema.

---
+++ DataJuggler.Net.NugetDemo/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DataJuggler.Net.NugetDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ConnectionStringControl = new DataJuggler.Win.Controls.LabelTextBoxControl();
+            this.GetSchemaHashButton = new System.Windows.Forms.Button();
+            this.SchemaHashControl = new DataJuggler.Win.Controls.LabelTextBoxControl();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConnectionStringControl
+            // 
+            this.ConnectionStringControl.BackColor = System.Drawing.Color.Transparent;
+            this.ConnectionStringControl.BottomMargin = 0;
+            this.ConnectionStringControl.Editable = true;
+            this.ConnectionStringControl.Encrypted = false;
+            this.ConnectionStringControl.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectionStringControl.LabelBottomMargin = 0;
+            this.ConnectionStringControl.LabelColor = System.Drawing.Color.LemonChiffon;
+            this.ConnectionStringControl.LabelFont = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.ConnectionStringControl.LabelText = "Conn String:";
+            this.ConnectionStringControl.LabelTopMargin = 0;
+            this.ConnectionStringControl.LabelWidth = 160;
+            this.ConnectionStringControl.Location = new System.Drawing.Point(27, 19);
+            this.ConnectionStringControl.MultiLine = false;
+            this.ConnectionStringControl.Name = "ConnectionStringControl";
+            this.ConnectionStringControl.OnTextChangedListener = null;
+            this.ConnectionStringControl.PasswordMode = false;
+            this.ConnectionStringControl.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.ConnectionStringControl.Size = new System.Drawing.Size(731, 32);
+            this.ConnectionStringControl.TabIndex = 0;
+            this.ConnectionStringControl.TextBoxBottomMargin = 0;
+            this.ConnectionStringControl.TextBoxDisabledColor = System.Drawing.Color.LightGray;
+            this.ConnectionStringControl.TextBoxEditableColor = System.Drawing.Color.White;
+            this.ConnectionStringControl.TextBoxFont = new System.Drawing.Font("Verdana", 12F);
+            this.ConnectionStringControl.TextBoxTopMargin = 0;
+            // 
+            // GetSchemaHashButton
+            // 
+            this.GetSchemaHashButton.BackColor = System.Drawing.Color.Transparent;
+            this.GetSchemaHashButton.BackgroundImage = global::DataJuggler.Net.NugetDemo.Properties.Resources.WoodButtonWidth2560;
+            this.GetSchemaHashButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GetSchemaHashButton.FlatAppearance.BorderSize = 0;
+            this.GetSchemaHashButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.GetSchemaHashButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.GetSchemaHashButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GetSchemaHashButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetSchemaHashButton.Location = new System.Drawing.Point(187, 58);
+            this.GetSchemaHashButton.Name = "GetSchemaHashButton";
+            this.GetSchemaHashButton.Size = new System.Drawing.Size(215, 39);
+            this.GetSchemaHashButton.TabIndex = 1;
+            this.GetSchemaHashButton.Text = "Get Schema Hash";
+            this.GetSchemaHashButton.UseVisualStyleBackColor = false;
+            this.GetSchemaHashButton.Click += new System.EventHandler(this.GetSchemaHashButton_Click);
+            this.GetSchemaHashButton.MouseEnter += new System.EventHandler(this.Button_Enter);
+            this.GetSchemaHashButton.MouseLeave += new System.EventHandler(this.Button_Leave);
+            // 
+            // SchemaHashControl
+            // 
+            this.SchemaHashControl.BackColor = System.Drawing.Color.Transparent;
+            this.SchemaHashControl.BottomMargin = 0;
+            this.SchemaHashControl.Editable = true;
+            this.SchemaHashControl.Encrypted = false;
+            this.SchemaHashControl.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SchemaHashControl.LabelBottomMargin = 0;
+            this.SchemaHashControl.LabelColor = System.Drawing.Color.LemonChiffon;
+            this.SchemaHashControl.LabelFont = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.SchemaHashControl.LabelText = "Schema Hash:";
+            this.SchemaHashControl.LabelTopMargin = 0;
+            this.SchemaHashControl.LabelWidth = 160;
+            this.SchemaHashControl.Location = new System.Drawing.Point(27, 110);
+            this.SchemaHashControl.MultiLine = false;
+            this.SchemaHashControl.Name = "SchemaHashControl";
+            this.SchemaHashControl.OnTextChangedListener = null;
+            this.SchemaHashControl.PasswordMode = false;
+            this.SchemaHashControl.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SchemaHashControl.Size = new System.Drawing.Size(375, 32);
+            this.SchemaHashControl.TabIndex = 2;
+            this.SchemaHashControl.TextBoxBottomMargin = 0;
+            this.SchemaHashControl.TextBoxDisabledColor = System.Drawing.Color.LightGray;
+            this.SchemaHashControl.TextBoxEditableColor = System.Drawing.Color.White;
+            this.SchemaHashControl.TextBoxFont = new System.Drawing.Font("Verdana", 12F);
+            this.SchemaHashControl.TextBoxTopMargin = 0;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.BackColor = System.Drawing.Color.Transparent;
+            this.CopyButton.BackgroundImage = global::DataJuggler.Net.NugetDemo.Properties.Resources.WoodButtonWidth2560;
+            this.CopyButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CopyButton.FlatAppearance.BorderSize = 0;
+            this.CopyButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.CopyButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.CopyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CopyButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopyButton.Location = new System.Drawing.Point(408, 103);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(108, 39);
+            this.CopyButton.TabIndex = 3;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = false;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            this.CopyButton.MouseEnter += new System.EventHandler(this.Button_Enter);
+            this.CopyButton.MouseLeave += new System.EventHandler(this.Button_Leave);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::DataJuggler.Net.NugetDemo.Properties.Resources.Deep_Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 175);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.SchemaHashControl);
+            this.Controls.Add(this.GetSchemaHashButton);
+            this.Controls.Add(this.ConnectionStringControl);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataJuggler.Net.Nuget Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.Controls.LabelTextBoxControl ConnectionStringControl;
+        private System.Windows.Forms.Button GetSchemaHashButton;
+        private Win.Controls.LabelTextBoxControl SchemaHashControl;
+        private System.Windows.Forms.Button CopyButton;
+    }
+}
+
---
+++ DataJuggler.Net.NugetDemo/MainForm.cs
@@ -0,0 +1,153 @@
+﻿
+
+#region using statements
+
+using DataJuggler.Core.UltimateHelper;
+using DataJuggler.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#endregion
+
+namespace DataJuggler.Net.NugetDemo
+{
+
+    #region class MainForm
+    /// <summary>
+    /// This form is used to test the GetDatbaseSchemaHash button
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        
+        #region Private Variables
+        #endregion
+        
+        #region Constructor
+        /// <summary>
+        /// Create a new instance of a 'MainForm' object.
+        /// </summary>
+        public MainForm()
+        {
+            // Create Controls
+            InitializeComponent();
+
+            // Init
+            Init();
+        }
+        #endregion
+        
+        #region Events
+            
+            #region Button_Enter(object sender, EventArgs e)
+            /// <summary>
+            /// event is fired when Button _ Enter
+            /// </summary>
+            private void Button_Enter(object sender, EventArgs e)
+            {
+                // Change the cursor to a hand
+                Cursor = Cursors.Hand;
+            }
+            #endregion
+            
+            #region Button_Leave(object sender, EventArgs e)
+            /// <summary>
+            /// event is fired when Button _ Leave
+            /// </summary>
+            private void Button_Leave(object sender, EventArgs e)
+            {
+                // Change the cursor back to the default pointer
+                Cursor = Cursors.Default;
+            }
+            #endregion
+
+            #region CopyButton_Click(object sender, EventArgs e)
+            /// <summary>
+            /// event is fired when the 'CopyButton' is clicked.
+            /// </summary>
+            private void CopyButton_Click(object sender, EventArgs e)
+            {
+                // Copy to your clipboard
+                Clipboard.SetText(SchemaHashControl.Text);
+
+                // Show the user a message
+                MessageBoxHelper.ShowMessage("The Schema Hash has been copied to your clipboard.", "Schema Hash Copied");
+            }
+            #endregion
+            
+            #region GetSchemaHashButton_Click(object sender, EventArgs e)
+            /// <summary>
+            /// event is fired when the 'GetSchemaHashButton' is clicked.
+            /// </summary>
+            private void GetSchemaHashButton_Click(object sender, EventArgs e)
+            {
+                // create a database
+                Database database = new Database();
+
+                // paste in your connectionstring
+                database.ConnectionString = ConnectionStringControl.Text;
+
+                try
+                {
+                    // if the connecionstring exists
+                    if (TextHelper.Exists(database.ConnectionString))
+                    {
+                        // Create a new instance of a 'SQLDatabaseConnector' object.
+                        SQLDatabaseConnector connector = new SQLDatabaseConnector();
+
+                        // set the connectionstring on the connector
+                        connector.ConnectionString = database.ConnectionString;
+
+                        // open the connection
+                        connector.Open();
+
+                        // get the schemaHash
+                        string schemaHash = connector.GetDatabaseSchemaHash(database);
+
+                        // close the connection
+                        connector.Close();
+
+                        // Display the result
+                        this.SchemaHashControl.Text = schemaHash;
+                    }
+                    else
+                    {
+                        // Show the user a message
+                        MessageBoxHelper.ShowMessage("You must paste in a connection string to continue.", "Connectionstring Required");
+                    }
+                }
+                catch (Exception error)
+                {
+                    // Show the user a message
+                    MessageBoxHelper.ShowMessage("An error occurred: " + error.ToString(), "Error");
+                }
+            }
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+            #region Init()
+            /// <summary>
+            /// This method  This method performs initializations for this object.
+            /// </summary>
+            public void Init()
+            {
+                // set the connection string value if set
+                this.ConnectionStringControl.Text = ConfigurationHelper.ReadAppSetting("Connectionstring");
+            }
+        #endregion
+
+        #endregion
+
+    }
+    #endregion
+
+}
