3a80d7d200f8ea336c5844289fa841c57f0c9b57
Data Juggler <corby@datajuggler.com>
2019-10-17 12:23:01 -0500

    I updated GetSchemaHash to export the full schema to xml, convert that value to a number, then encrypt it.
    This is a longer string, but a more accurate hash is created.

--- DataJuggler.Net.NugetDemo/Properties/AssemblyInfo.cs
+++ DataJuggler.Net.NugetDemo/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataJuggler.Net.NugetDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple project used to get the database schema, plus I also use this project to get the latest commit hash for DataTier.Net Git Hub repo.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Data Juggler")]
 [assembly: AssemblyProduct("DataJuggler.Net.NugetDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
